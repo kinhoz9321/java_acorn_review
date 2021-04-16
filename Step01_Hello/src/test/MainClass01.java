1bffe85e2d159196803072f44ade9c1fc0a7ae8a
kimhongju <kinhoz9321@gmail.com>
2021-04-16 14:08:11 +0900

    Step01_Hello 프로젝트 추가 (+gitignore)

---
+++ Step01_Hello/src/test/MainClass01.java
@@ -0,0 +1,18 @@
+package test;
+
+//MainClass 라는 이름의 Class 생성
+public class MainClass01 {
+	
+	//main 이라는 이름의 static 메소드 생성
+	public static void main(String[] args) {
+		
+		//syso : 콘솔창에 "Hello, world!" 출력
+		System.out.println("Hello, World!");
+		
+	}
+}
+/*
+	Tip!
+	main 메소드 자동완성 : main + Ctrl + Spacebar 
+	System.out.println() 자동완성 : (syso or sysout) + Ctrl + Spacebar
+*/
---
+++ Step01_Hello/src/test/MainClass02.java
@@ -0,0 +1,16 @@
+package test;
+
+public class MainClass02 {
+	
+	//run 했을 때 실행의 흐름이 시작되는 특별한 main 메소드!
+	//실행의 흐름이 시작된다는 뜻은 main 메소드의 위에서 아래로 코드가 실행된다는 뜻이다.
+	public static void main(String[] args) {
+		System.out.println("자바 기초 복습 중");
+		System.out.println("콘솔창에");
+		System.out.println("파워 출력!");
+	}
+}
+/*
+ 	Tip!
+ 	이클립스 자동 줄바꿈 단축키 : Alt + Shift + y
+ */
